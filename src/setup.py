2b9d22b11876bddd35220a0d804fc6a12627349e
Scott Sibole <scott.sibole@gmail.com>
2016-05-27 16:47:38 -0600

    Updated setup script to handle platform-specific package_data inclusion.

--- src/setup.py
+++ src/setup.py
@@ -1,20 +1,28 @@
 from setuptools import setup, find_packages
 from setuptools.dist import Distribution
 import os
+import sys
 
 class BinaryDistribution(Distribution):
     def is_pure(self):
         return False
 
-if os.name == "posix":
+if "win_amd64" in sys.argv:
+    win64 = True
+else:
+    win64=False
+
+if os.name == "posix" and not win64:
     package_data = {"tetmesh": ["linux/*"]}
-elif os.name == "nt":
+elif os.name == "nt" or win64:
+    print "This"
     package_data = {"tetmesh": ["windows/*"]}
 
 setup(
     name = "tetmesh",
     version = "0.0",
     packages = find_packages(),
+    package_dir={"tetmesh": "tetmesh"},
     include_package_data=True,
     package_data=package_data,
     distclass=BinaryDistribution,
