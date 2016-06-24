381dc9d3749b2803d37cf7573569806d76c53f1d
Scott Sibole <scott.sibole@gmail.com>
2016-06-24 17:11:34 -0600

    Added full path to nmake as CMAKE flag.

--- src/bld.bat
+++ src/bld.bat
@@ -2,7 +2,7 @@ set CMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
 
 cd %~dp0src/cpp
 
-cmake -DCMAKE_BUILD_TYPE=Release -G"NMake Makefiles"
+cmake -DCMAKE_BUILD_TYPE=Release -G"NMake Makefiles" -DCMAKE_CXX_COMPILER="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\nmake.exe"
 if errorlevel 1 exit 1
 
 nmake
