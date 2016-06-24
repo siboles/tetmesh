80894a0de2dd8d9195523c71521e953bb7d630c6
Scott Sibole <scott.sibole@gmail.com>
2016-06-24 14:25:38 -0600

    More changes to function with conda.

--- src/build.sh
+++ src/build.sh
@@ -3,6 +3,8 @@ cd src/cpp
 cgal_create_CMakeLists -s executable
 cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_C_COMPILER=/opt/conda/bin/gcc .
+make
+make install
 cd ..
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
 
