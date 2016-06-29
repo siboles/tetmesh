67fb17dd058838e3035b6f97d4cf3a9fb41b6a40
Scott Sibole <scott.sibole@gmail.com>
2016-06-28 20:14:23 -0600

    Attempt to work with Visual Studio

--- src/bld.bat
+++ src/bld.bat
@@ -1,14 +1,19 @@
 set CMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
+cd %~dp0cpp
+if errorlevel 1 exit 1
 
-cd %~dp0src/cpp
-
-cmake -DCMAKE_BUILD_TYPE=Release -G"NMake Makefiles" -DCMAKE_CXX_COMPILER="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\nmake.exe"
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" -G"NMake Makefiles" .
 if errorlevel 1 exit 1
 
 nmake
 if errorlevel 1 exit 1
-move mesh_polyhedral_domain.exe %~dp0 
+nmake install
+if errorlevel 1 exit 1
 
+move mesh_polyhedral_domain.exe %~dp0
+if errorlevel 1 exit 1
 cd %~dp0
+if errorlevel 1 exit 1
 
 %PYTHON% setup.py install
+if errorlevel 1 exit 1
