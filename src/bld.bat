3e35fc4e8c6b2f4df12c8cd230c0635dbf249d1f
Scott Sibole <scott.sibole@gmail.com>
2016-07-08 12:45:24 -0600

    Working conda build for Windows.

--- src/bld.bat
+++ src/bld.bat
@@ -1,18 +1,15 @@
-set CMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
-cd %~dp0cpp
+cd %~dp0src/cpp
 if errorlevel 1 exit 1
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" -G"NMake Makefiles" .
+cmake -DCMAKE_BUILD_TYPE=Release -G"NMake Makefiles" .
 if errorlevel 1 exit 1
 
 nmake
 if errorlevel 1 exit 1
-nmake install
-if errorlevel 1 exit 1
 
-move mesh_polyhedral_domain.exe %~dp0
+move mesh_polyhedral_domain.exe %~dp0src/tetmesh
 if errorlevel 1 exit 1
-cd %~dp0
+cd %~dp0src
 if errorlevel 1 exit 1
 
 %PYTHON% setup.py install
