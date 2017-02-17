1e2618b023fec1a8cc2df18de26bd34ddd6fb4a3
Scott Sibole <scott.sibole@gmail.com>
2017-02-17 16:10:21 -0700

    More build issues.

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 cd src/cpp
 cmake -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_SYSTEM_PREFIX_PATH=$PREFIX \
+      -D BOOST_ROOT=$PREFIX -D Boost_NO_SYSTEM_PATHS=ON \
       -DCMAKE_C_COMPILER=$HOME/anaconda2/bin/gcc .
 make
 mv mesh_polyhedral_domain ../tetmesh/
